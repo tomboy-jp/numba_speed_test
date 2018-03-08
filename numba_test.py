e394eff40d894bbe1001fcb4f88753764588d63b
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-09 00:20:27 +0900

    add new function exe_time_printer and import pandas

--- numba_test.py
+++ numba_test.py
@@ -1,38 +1,53 @@
-from time import time
 import numpy as np
+import pandas as pd
+from time import time
 from numba import jit
 
-def numpy_only(x, y):
+def numpy_only(x_num, y_num):
 
-    result = 0
-    X = np.random.randn(x)
-    Y = np.random.randn(y)
+    result = float(0)
+    X = np.random.rand(x_num)
+    Y = np.random.rand(y_num)
     for x in X:
         for y in Y:
-            result += x + y
+            result += x - y
 
     return result
 
 @jit
 def numba_jit(x, y):
 
-    result = 0
-    X = np.random.randn(x)
-    Y = np.random.randn(y)
+    result = float(0)
+    X = np.random.rand(x)
+    Y = np.random.rand(y)
     for x in X:
         for y in Y:
-            result += x + y
+            result += x - y
 
     return result
 
-print('numpy_only, start!')
-start = time()
-numpy_only(10000, 10000)
-end = time()
-print('exe_time: {}[sec]'.format(end - start) + '\n')
+def exe_time_printer(x, y):
 
-print('numba_jit, start!')
-start = time()
-numba_jit(10000, 10000)
-end = time()
-print('exe_time: {}[sec]'.format(end - start) + '\n')
+    print('x = ' + str(x) + ', y = ' + str(y) + '\n')
+    print('numpy_only, start!')
+    start = time()
+    numpy_only(x, y)
+    np_exe_time = time() - start
+    print('exe_time: {}[sec]'.format(np_exe_time) + '\n')
+
+    print('numba_jit, start!')
+    start = time()
+    numba_jit(x, y)
+    nb_exe_time = time() - start
+    print('exe_time: {}[sec]'.format(nb_exe_time) + '\n')
+
+    return np_exe_time, nb_exe_time
+
+li = [1, 10, 100, 1000, 10000]
+df = pd.DataFrame(index=['np_exe_time','nb_exe_time'])
+
+for i in li:
+    np_exe_time, nb_exe_time = exe_time_printer(i, i)
+    df[str(i)] = [str(np_exe_time) + '[sec]', str(nb_exe_time) + '[sec]']
+
+print(df)
