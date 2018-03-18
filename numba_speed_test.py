a80bafff5a50ce10b3725c0db67144ee594966c8
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-18 11:10:52 +0900

    update

--- numba_speed_test.py
+++ numba_speed_test.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 from time import time
 from numba import jit
 
-def numpy_only(num):
+def jit_off(num):
+    '''
+    numbaを使わない方の関数です。
+    '''
 
     result = float(0)
-    X = np.random.rand(num)
-    Y = np.random.rand(num)
+    X = np.random.rand(freq)
+    Y = np.random.rand(freq)
     for x in X:
         for y in Y:
             result += x - y
@@ -16,39 +19,46 @@ def numpy_only(num):
     return result
 
 @jit
-def numba_jit(num):
+def jit_on(freq):
+    '''
+    numbaを使う方の関数です。
+    '''
 
     result = float(0)
-    X = np.random.rand(num)
-    Y = np.random.rand(num)
+    X = np.random.rand(freq)
+    Y = np.random.rand(freq)
     for x in X:
         for y in Y:
             result += x - y
 
     return result
 
-def exe_time_printer(num):
+def exe_time_printer(freq):
+    '''
+    2つの関数に係数freq(実行回数:freq**2)を引き渡してそれぞれの実行時間を10進数のstr型で出力する関数です。
+    '''
 
     start = time()
-    numpy_only(num)
-    np_exe_time = time() - start
+    jit_off(freq)
+    exe_time_jit_off = time() - start
 
     start = time()
-    numba_jit(num)
-    nb_exe_time = time() - start
+    jit_on(freq)
+    exe_time_jit_on = time() - start
 
-    return "%.10f"%np_exe_time, "%.10f"%nb_exe_time
+    return "%.10f"%exe_time_jit_off, "%.10f"%exe_time_jit_on
 
-freq = np.arange(0,10001,100)
-df = pd.DataFrame(columns=['freq','np_exe_time','nb_exe_time'])
+frequencies = np.arange(0, 10001, 100)
+df = pd.DataFrame(columns=['freq','exe_time_jit_off','exe_time_jit_on'])
 
-for i in freq:
-    # print(i)
-    np_exe_time, nb_exe_time = exe_time_printer(int(i))
-    se = pd.Series([i, float(np_exe_time[:7]), float(nb_exe_time[:7])], index=df.columns)
+for freq in frequencies:
+    print(freq)
+    exe_time_jit_off, exe_time_jit_on = exe_time_printer(int(freq))
+    se = pd.Series([freq, float(exe_time_jit_off[:8]), float(exe_time_jit_on[:8])], index=df.columns)
     df = df.append(se, ignore_index=True)
 
 df.to_csv('result/result.tsv', sep='\t', index=False)
-df.plot(x='freq', y=['np_exe_time', 'nb_exe_time'], colormap='jet', marker='.', markersize=10, title='Numba Speed Test', figsize=(10,5), alpha=0.5)
+df.plot(x='freq', y=['exe_time_jit_off', 'exe_time_jit_on'], colormap='jet', marker='.', markersize=10, title='Numba Speed Test', figsize=(10,5), alpha=0.5)
+
 plt.savefig('result/result.png')
 plt.pause(5)
