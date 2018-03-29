8d479d408ebfa03fb040d9e20200d2ab076b2ffe
tomboy <tomboy-jp@users.noreply.github.com>
2018-03-30 00:11:44 +0900

    correction

--- numba_speed_test.py
+++ numba_speed_test.py
@@ -8,6 +8,7 @@ def jit_off(num):
     '''
     numbaを使わない方の関数です。
     '''
+
     result = float(0)
     X = np.random.rand(freq)
     Y = np.random.rand(freq)
@@ -22,6 +23,7 @@ def jit_on(freq):
     '''
     numbaを使う方の関数です。
     '''
+
     result = float(0)
     X = np.random.rand(freq)
     Y = np.random.rand(freq)
@@ -35,27 +37,24 @@ def exe_time_printer(freq):
     '''
     2つの関数に係数freq(実行回数=freq**2)を引き渡してそれぞれの実行時間を10進数のstr型で出力する関数です。
     '''
-    np.set_printoptions(precision=8, suppress=True)
 
     start = time()
     jit_off(freq)
     exe_time_jit_off = time() - start
 
-
     start = time()
     jit_on(freq)
     exe_time_jit_on = time() - start
 
-    # return "%.10f"%exe_time_jit_off, "%.10f"%exe_time_jit_on
-    return exe_time_jit_off.astype(np.float), exe_time_jit_on.astype(np.float)
+    return "%.10f"%exe_time_jit_off, "%.10f"%exe_time_jit_on
 
 frequency = np.arange(0, 10001, 100)
 df = pd.DataFrame(columns=['freq','exe_time_jit_off','exe_time_jit_on'])
 
 for freq in frequency:
     print(freq)
-    exe_time_jit_off, exe_time_jit_on = exe_time_printer(freq)
-    se = pd.Series([freq, exe_time_jit_off, exe_time_jit_on], index=df.columns)
+    exe_time_jit_off, exe_time_jit_on = exe_time_printer(int(freq))
+    se = pd.Series([freq, float(exe_time_jit_off[:8]), float(exe_time_jit_on[:8])], index=df.columns)
     df = df.append(se, ignore_index=True)
 
 df.to_csv('result/result.tsv', sep='\t', index=False)
